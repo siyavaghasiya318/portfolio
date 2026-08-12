3e98afc764fc01ba22fac68c349ea719ffb0fc05
siyavaghasiya318 <siyavaghasiya318@gmail.com>
2026-08-12 15:16:31 +0530

    Update package.json scripts and UserContext API endpoint for development

--- my-portfolio/src/Context/UserContext.jsx
+++ my-portfolio/src/Context/UserContext.jsx
@@ -3,46 +3,48 @@ import axios from "axios"
 import { toast } from "react-hot-toast";
 export const userContax = createContext()
 
-export const UserProvider = ({children}) => {
+const API = "http://localhost:5000/api"
+// const API = "https://portfolio-r05h.onrender.com/api"
 
-    // const API = "http://localhost:5000/api"
-    const API = "https://portfolio-r05h.onrender.com"
-    const[sendContact,setSendContact] = useState({
-        name:"",
-        email:"",
-        message:""
+export const UserProvider = ({ children }) => {
+
+
+    const [sendContact, setSendContact] = useState({
+        name: "",
+        email: "",
+        message: ""
     })
-    
-    const HandleChange = (e) =>{
-       const{name,value} = e.target
+
+    const HandleChange = (e) => {
+        const { name, value } = e.target        
         setSendContact({
             ...sendContact,
-            [name]:value
+            [name]: value
         })
     }
 
-    const submitContactForm = async(e) => {
+    const submitContactForm = async (e) => {
         e.preventDefault()
-        
+
         try {
-            const{data} = await axios.post(`${API}/contact/detail`, sendContact)
+            const { data } = await axios.post(`${API}/contact/detail`, sendContact)
             console.log(data.message);
             toast.success(data.message)
 
             setSendContact({
-                name:"",
-                email:"",
-                message:""
+                name: "",
+                email: "",
+                message: ""
             })
-            
+
         } catch (error) {
             console.log("submitContact error", error);
-            toast.error(error?.response?.data?.message  || "something went wrong");
+            toast.error(error?.response?.data?.message || "something went wrong");
         }
     }
 
-    return(
-        <userContax.Provider value={{HandleChange,submitContactForm,sendContact}}>
+    return (
+        <userContax.Provider value={{ HandleChange, submitContactForm, sendContact }}>
             {children}
         </userContax.Provider>
     )
